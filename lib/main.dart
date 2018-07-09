7dc0079afc89d0b8757f13309bcaa43609474ab4
Nolan Brady <nolanrbrady@gmail.com>
2018-07-08 23:03:17 -0600

    ListView Rendering

--- lib/main.dart
+++ lib/main.dart
@@ -36,26 +36,79 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-           Text('Boulder', style: new TextStyle(fontSize: 20.0)),
-           Text('95 degrees', style: new TextStyle(fontSize: 20.0)),
-           Image.network('https://openweathermap.org/img/w/01d.png'),
-           Text('July 07, 2018', style: new TextStyle(color: Colors.black)),
-            Text('18:30', style: new TextStyle(color: Colors.black)),
+        appBar: new AppBar(
+          title: new Text(widget.title),
+          backgroundColor: Colors.blueGrey,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              color: Colors.black,
+              onPressed: () {},
+            )
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: new Stack(
+                        children: <Widget>[
+                          new Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text('Boulder',
+                                  style: new TextStyle(fontSize: 20.0)),
+                              Text('95 degrees',
+                                  style: new TextStyle(fontSize: 20.0)),
+                              Image.network(
+                                  'https://openweathermap.org/img/w/01d.png'),
+                              Text('July 07, 2018',
+                                  style: new TextStyle(color: Colors.black)),
+                              Text('18:30',
+                                  style: new TextStyle(color: Colors.black)),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SafeArea(
+                child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Container(
+                        height: 200.0,
+                        child: ListView.builder(
+                            itemCount: 10,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) => Card(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text('Boulder', style: new TextStyle(fontSize: 20.0)),
+                                        Text('95 degrees', style: new TextStyle(fontSize: 20.0)),
+                                        Image.network('https://openweathermap.org/img/w/01d.png'),
+                                        Text('July 07, 2018', style: new TextStyle(color: Colors.black)),
+                                        Text('18:30', style: new TextStyle(color: Colors.black)),
+                                      ],
+                                    ),
+                                  ),
+                                )))),
+              )
+            ],
+          ),
+        ));
   }
 }
